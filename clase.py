4310d362ba6c2a868fdd7e87d7b90b9d6d132856
miriii00 <mirandatrevino21alvarez@gmail.com>
2026-04-20 18:51:29 -0600

    se agregaron los atributos privados

--- clase.py
+++ clase.py
@@ -1,2 +1,6 @@
 class Mascota:
-    pass
+    def __init__(self, nombre, especie, edad, peso):
+        self.__nombre = nombre
+        self.__especie = especie
+        self.__edad = edad
+        self.__peso = peso
