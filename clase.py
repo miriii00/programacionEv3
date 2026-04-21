315a9a649436064f5a29f21103142dbdd3b0c565
miriii00 <mirandatrevino21alvarez@gmail.com>
2026-04-20 18:52:51 -0600

    se agregaron los getters

--- clase.py
+++ clase.py
@@ -4,3 +4,14 @@ class Mascota:
         self.__especie = especie
         self.__edad = edad
         self.__peso = peso
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_especie(self):
+        return self.__especie
+
+    def get_edad(self):
+        return self.__edad
+
+    def get_peso(self):
+        return self.__peso
